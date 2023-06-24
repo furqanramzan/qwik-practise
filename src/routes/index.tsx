cbae2e5dca5b05fcdf88b2d55b9b194549025693
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-06-24 23:39:37 +0500

    feat: add tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,18 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <section class="flex h-screen items-center justify-center bg-white dark:bg-gray-900">
+        <div class="mx-auto px-4 py-8 text-center lg:px-12 lg:py-16">
+          <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+            Hi 👋
+          </h1>
+          <p class="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+            Can't wait to see what you build with qwik!
+            <br />
+            Happy coding.
+          </p>
+        </div>
+      </section>
     </>
   );
 });
